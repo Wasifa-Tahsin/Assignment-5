e6969c4767e85c4d03f4c7e7ee49a80fcd283e99
Afia <arminakterafia@gmail.com>
2024-09-23 15:25:52 +0600

    practice

---
+++ js/home.js
@@ -0,0 +1,12 @@
+function getInputFieldValueById(id){
+    const inputValue=document.getElementById(id).value
+    const inputNumber=parseFloat(inputValue)
+    // console.log(inputNumber)
+    return inputNumber;
+    
+    }
+    function getTheElementId(id){
+        const elementId=document.getElementById(id).innerText
+        const result=parseFloat(elementId)
+        return result;
+    }
--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-document.getElementById('domation-btn').addEventListener('click',function(){
-    
+// the Noakhali donation btn
+document.getElementById('btn-flood-Noakhali').addEventListener('click',function(){
+   const balance= getInputFieldValueById('input-flood-Noakhali')
+  
+   const accountBalance=getTheElementId('mainBalance')
+   const newBalance=accountBalance-balance;
+   document.getElementById('mainBalance').innerText=newBalance
+document.getElementById('Noakhali-donation').innerText=balance
+
 })
+
