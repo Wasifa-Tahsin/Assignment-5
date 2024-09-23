cbd5163b1de4d5e2a0cb4e0688a8e9d7ce46d4eb
Afia <arminakterafia@gmail.com>
2024-09-23 15:45:36 +0600

    practice

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// the Noakhali donation btn
+// Noakhali donation 
 document.getElementById('btn-flood-Noakhali').addEventListener('click',function(){
    const balance= getInputFieldValueById('input-flood-Noakhali')
   
@@ -9,3 +9,22 @@ document.getElementById('Noakhali-donation').innerText=balance
 
 })
 
+// Feni Donation
+document.getElementById('feni-donation-btn').addEventListener('click',function(){
+   const inputFeniDonation=getInputFieldValueById('input-feni-donation')
+   const accountBalance=getTheElementId('mainBalance')
+   const newBalance=accountBalance-inputFeniDonation;
+   document.getElementById('mainBalance').innerText=newBalance
+   document.getElementById('feni-donation').innerText=inputFeniDonation
+
+})
+
+// Quota Movement
+document.getElementById('quota-movement-btn').addEventListener('click',function(){
+   const inputQuotaMovement=getInputFieldValueById('input-quota-movement')
+   const accountBalance=getTheElementId('mainBalance')
+   const newBalance=accountBalance-inputQuotaMovement;
+   document.getElementById('mainBalance').innerText=newBalance
+   document.getElementById('quota-movement').innerText=inputQuotaMovement
+
+})
